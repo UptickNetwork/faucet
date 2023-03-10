0de518da16bd623aeb4b31da9f1fe343439bd230
wangxiaolei <wangxiaolei@897672818@qq.com>
2023-03-10 17:04:03 +0800

    新建项目

---
+++ config.js
@@ -0,0 +1,209 @@
+
+import { stringToPath } from '@cosmjs/crypto'
+
+export default {
+    port: 8181, // http port 
+    db: {
+        path: "./db/faucet.db" // save request states 
+    },
+    project: {
+        name: "Game of NFT",
+        logo: "https://interchainnfts.dev/assets/img/banner.e3623744.png",
+        deployer: `<a href="#">Ping.pub</a>`
+    },
+    blockchains: [
+        {
+            name: "gon-irishub-1",
+            endpoint: {
+                // make sure that CORS is enabled in rpc section in config.toml
+                // cors_allowed_origins = ["*"]
+                rpc_endpoint: " http://34.80.93.133:26657",
+            },
+            sender: {
+                mnemonic: "surround miss nominee dream gap cross assault thank captain prosper drop duty group candy wealth weather scale put",
+                option: {
+                    hdPaths: [stringToPath("m/44'/118'/0'/0/0")],
+                    prefix: "iaa"
+                }
+            },
+            tx: {
+                amount: {
+                    denom: "uiris",
+                    amount: "10000000"
+                },
+                fee: {
+                    amount: [
+                        {
+                            amount: "5000",
+                            denom: "uiris"
+                        }
+                    ],
+                    gas: "200000"
+                },
+            },
+            limit: {
+                // how many times each wallet address is allowed in a window(24h)
+                address: 1, 
+                // how many times each ip is allowed in a window(24h),
+                // if you use proxy, double check if the req.ip is return client's ip.
+                ip: 10 
+            }
+        },
+        {
+            name: "Stargaze-elgafar-1",
+            endpoint: {
+                // make sure that CORS is enabled in rpc section in config.toml
+                // cors_allowed_origins = ["*"]
+                rpc_endpoint: "https://rpc.elgafar-1.stargaze-apis.com:443",
+            },
+            sender: {
+                mnemonic: "surround miss nominee dream gap cross assault thank captain prosper drop duty group candy wealth weather scale put",
+                option: {
+                    hdPaths: [stringToPath("m/44'/118'/0'/0/0")],
+                    prefix: "stars"
+                }
+            },
+            tx: {
+                amount: {
+                    denom: "ustars",
+                    amount: "10000000"
+                },
+                fee: {
+                    amount: [
+                        {
+                            amount: "100000",
+                            denom: "ustars"
+                        }
+                    ],
+                    gas: "200000"
+                },
+            },
+            limit: {
+                // how many times each wallet address is allowed in a window(24h)
+                address: 1, 
+                // how many times each ip is allowed in a window(24h),
+                // if you use proxy, double check if the req.ip is return client's ip.
+                ip: 10 
+            }
+        },
+        {
+            name: "Juno-uni-6",
+            endpoint: {
+                // make sure that CORS is enabled in rpc section in config.toml
+                // cors_allowed_origins = ["*"]
+                rpc_endpoint: "https://rpc.uni.junonetwork.io/",
+            },
+            sender: {
+                mnemonic: "surround miss nominee dream gap cross assault thank captain prosper drop duty group candy wealth weather scale put",
+                option: {
+                    hdPaths: [stringToPath("m/44'/118'/0'/0/0")],
+                    prefix: "juno"
+                }
+            },
+            tx: {
+                amount: {
+                    denom: "ujunox",
+                    amount: "10000000"
+                },
+                fee: {
+                    amount: [
+                        {
+                            amount: "100000",
+                            denom: "ujunox"
+                        }
+                    ],
+                    gas: "200000"
+                },
+            },
+            limit: {
+                // how many times each wallet address is allowed in a window(24h)
+                address: 1, 
+                // how many times each ip is allowed in a window(24h),
+                // if you use proxy, double check if the req.ip is return client's ip.
+                ip: 10 
+            }
+        },
+        {
+            name: "gon-flixnet-1",
+            endpoint: {
+                // make sure that CORS is enabled in rpc section in config.toml
+                // cors_allowed_origins = ["*"]
+                rpc_endpoint: "http://65.21.93.56:26657/",
+            },
+            sender: {
+                mnemonic: "surround miss nominee dream gap cross assault thank captain prosper drop duty group candy wealth weather scale put",
+                option: {
+                    hdPaths: [stringToPath("m/44'/118'/0'/0/0")],
+                    prefix: "omniflix"
+                }
+            },
+            tx: {
+                amount: {
+                    denom: "uflix",
+                    amount: "10000000"
+                },
+                fee: {
+                    amount: [
+                        {
+                            amount: "100000",
+                            denom: "uflix"
+                        }
+                    ],
+                    gas: "200000"
+                },
+            },
+            limit: {
+                // how many times each wallet address is allowed in a window(24h)
+                address: 1, 
+                // how many times each ip is allowed in a window(24h),
+                // if you use proxy, double check if the req.ip is return client's ip.
+                ip: 10 
+            }
+        },
+        {
+            type: 'Ethermint',
+            ids: {
+                chainId: 7000,
+                cosmosChainId: 'uptickd_7000-2',
+            },
+            name: "uptickd_7000-2",
+            endpoint: {
+                // make sure that CORS is enabled in rpc section in config.toml
+                // cors_allowed_origins = ["*"]
+                rpc_endpoint: "http://52.220.252.160:26657/",
+                evm_endpoint: "http://52.220.252.160:8545/",
+            },
+            sender: {
+                mnemonic: "surround miss nominee dream gap cross assault thank captain prosper drop duty group candy wealth weather scale put",
+                option: {
+                    hdPaths: [stringToPath("m/44'/60/0'/0/0")],
+                    prefix: "uptick"
+                }
+            },
+            tx: {
+                amount: {
+                    denom: "auptick",
+                    amount: "5000000000000000000"
+                },
+                fee: {
+                    amount: [
+                        {
+                            amount: "100000",
+                            denom: "auptick"
+                        }
+                    ],
+                    gas: "10000000000000"
+                },
+            },
+            limit: {
+                // how many times each wallet address is allowed in a window(24h)
+                address: 1, 
+                // how many times each ip is allowed in a window(24h),
+                // if you use proxy, double check if the req.ip is return client's ip.
+                ip: 10 
+            }
+        },
+
+
+    ]    
+}
---
+++ faucet.js
@@ -0,0 +1,180 @@
+import express from 'express';
+import * as path from 'path'
+
+import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { SigningStargateClient } from "@cosmjs/stargate";
+import { FrequencyChecker } from './checker';
+
+import conf from './config'
+
+import { Wallet } from '@ethersproject/wallet'
+import { pathToString } from '@cosmjs/crypto';
+
+import { ethers } from 'ethers';
+import { bech32 } from 'bech32';
+
+// load config
+console.log("loaded config: ", conf)
+
+const app = express()
+
+const checker = new FrequencyChecker(conf)
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('./index.html'));
+})
+
+app.get('/config.json', async (req, res) => {
+  const sample = {}
+  for(let i =0; i < conf.blockchains.length; i++) {
+    const chainConf = conf.blockchains[i]
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(chainConf.sender.mnemonic, chainConf.sender.option);
+    const [firstAccount] = await wallet.getAccounts();
+    sample[chainConf.name] = firstAccount.address
+
+
+    pathToString
+    const wallet2 = Wallet.fromMnemonic(chainConf.sender.mnemonic, pathToString(chainConf.sender.option.hdPaths[0]));
+    console.log('address:', firstAccount.address, wallet2.address)
+  }
+
+  const project = conf.project
+  project.sample = sample
+  project.blockchains = conf.blockchains.map(x => x.name)
+  res.send(project);
+})
+
+app.get('/balance/:chain', async (req, res) => {
+  const { chain }= req.params
+
+  let balance = {}
+
+  const chainConf = conf.blockchains.find(x => x.name === chain)
+  if(chainConf.type === 'Ethermint') {
+    const ethProvider = new ethers.providers.JsonRpcProvider(chainConf.endpoint.evm_endpoint);
+    const wallet = Wallet.fromMnemonic(chainConf.sender.mnemonic).connect(ethProvider);
+    const ethBlance = await wallet.getBalance()
+
+    balance = {
+      denom:chainConf.tx.amount.denom,
+      amount:ethBlance.toString()
+    }
+  }else{
+
+    const rpcEndpoint = chainConf.endpoint.rpc_endpoint;
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(chainConf.sender.mnemonic, chainConf.sender.option);
+
+    const client = await SigningStargateClient.connectWithSigner(rpcEndpoint, wallet);
+    const [firstAccount] = await wallet.getAccounts();
+    balance = await client.getBalance(firstAccount.address, chainConf.tx.amount.denom);
+  }
+
+  res.send(balance);
+})
+
+app.get('/send/:chain/:address', async (req, res) => {
+  const {chain, address} = req.params;
+  const ip = req.headers['x-real-ip'] || req.headers['X-Real-IP'] || req.headers['X-Forwarded-For'] || req.ip
+  console.log('request tokens to ', address, ip)
+  if (chain || address ) {
+    try {
+      const chainConf = conf.blockchains.find(x => x.name === chain)
+      if (chainConf && address.startsWith(chainConf.sender.option.prefix)) {
+        if( await checker.checkAddress(address, chain) && await checker.checkIp(`${chain}${ip}`, chain) ) {
+          checker.update(`${chain}${ip}`) // get ::1 on localhost
+          sendTx(address, chain).then(ret => {
+
+            checker.update(address)
+            res.send({ result: ret })
+          }).catch(err => {
+            res.send({ result: `err: ${err}`})
+          });
+        }else {
+          res.send({ result: "You requested too often" })
+        }
+      } else {
+        res.send({ result: `Address [${address}] is not supported.` })
+      }
+    } catch (err) {
+      console.error(err);
+      res.send({ result: 'Failed, Please contact to admin.' })
+    }
+
+  } else {
+    // send result
+    res.send({ result: 'address is required' });
+  }
+})
+
+app.listen(conf.port, () => {
+  console.log(`Faucet app listening on port ${conf.port}`)
+})
+
+async function sendCosmosTx(recipient, chain) {
+  // const mnemonic = "surround miss nominee dream gap cross assault thank captain prosper drop duty group candy wealth weather scale put";
+  const chainConf = conf.blockchains.find(x => x.name === chain) 
+  if(chainConf) {
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(chainConf.sender.mnemonic, chainConf.sender.option);
+    const [firstAccount] = await wallet.getAccounts();
+
+    // console.log("sender", firstAccount);
+
+    const rpcEndpoint = chainConf.endpoint.rpc_endpoint;
+    const client = await SigningStargateClient.connectWithSigner(rpcEndpoint, wallet);
+    client.getBalance
+
+    // const recipient = "cosmos1xv9tklw7d82sezh9haa573wufgy59vmwe6xxe5";
+    const amount = chainConf.tx.amount;
+    const fee = chainConf.tx.fee;
+    return client.sendTokens(firstAccount.address, recipient, [amount], fee);
+  }
+  throw new Error(`Blockchain Config [${chain}] not found`)
+}
+
+async function sendEvmosTx(recipient, chain) {
+
+  const chainConf = conf.blockchains.find(x => x.name === chain) 
+  const ethProvider = new ethers.providers.JsonRpcProvider(chainConf.endpoint.evm_endpoint);
+
+  const wallet = Wallet.fromMnemonic(chainConf.sender.mnemonic).connect(ethProvider);
+  let decode = bech32.decode(recipient);
+  let array = bech32.fromWords(decode.words);
+  let evmAddress =  "0x" + toHexString(array);
+  try{
+    let result = await wallet.sendTransaction(
+        { 
+          from:wallet.address,
+          to:evmAddress,
+          value:chainConf.tx.amount.amount
+        }
+      );
+   
+    let repTx = {
+      "code":0,
+      "nonce":result["nonce"],
+      "value":result["value"].toString(),
+      "hash":result["hash"]
+    };
+
+    console.log("xxl result : ",repTx);
+    return repTx;
+  }catch(e){
+    console.log("xxl e ",e);
+    return e;
+  }
+
+}
+
+function toHexString(bytes) {
+  return bytes.reduce(
+      (str, byte) => str + byte.toString(16).padStart(2, '0'), 
+      '');
+}
+
+async function sendTx(recipient, chain) {
+  const chainConf = conf.blockchains.find(x => x.name === chain) 
+  if(chainConf.type === 'Ethermint') {
+    return sendEvmosTx(recipient, chain)
+  }
+  return sendCosmosTx(recipient, chain)
+}
